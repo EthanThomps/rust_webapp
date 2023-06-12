024a11da3ace959aa0aa55a5e40c570e41dc39b9
EthanThomps <81367830+EthanThomps@users.noreply.github.com>
2023-06-12 16:56:09 +0000

    bug occured!

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,8 @@ fn main() {
     dioxus_web::launch(App);
 }
 
-fn ToggleLight(cx: Scope) -> Element {
-    let mut toggle = true;
-
-    cx.render(rsx! {
-        button {
-            onclick: move |e| !toggle, 
-            "Toggle Variablee"
-        }
-
-        if toggle {
-            rsx!(div { "True"})
-            toggle = false
-        } else {
-            rsx!(div { "False"})
-            toggle = true
-        }
-
-    })
-}
-
-// create a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
     cx.render(rsx! {
-        ToggleLight {},
-        ToggleLight {}
+       div {"dockerized"}
     }) 
 }
