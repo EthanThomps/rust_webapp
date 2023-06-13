ef4597bcedb467efb694030adb381aaa3d60776a
EthanThomps <81367830+EthanThomps@users.noreply.github.com>
2023-06-13 14:39:23 +0000

    App dir fix!

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,26 +1,32 @@
-use axum::{
-    routing::get,
-    Router
-};
-
-
-
-
-
-
-
-
+use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    // build route
-    let app = Router::new().route("/", get(|| async {
-        "Route Build"
-    }));
-
+    // build routes
+    let app = Router::new()
+        .route("/", get(|| async { "index Page" }))
+        .route("/create-todos", get(create_todos))
+        .route("/delete-todos", get(delete_todos))
+        .route("/view-todos", get(view_todos));
+    // starts multi-threaded server
     axum::Server::bind(&"0.0.0.0:5000".parse().unwrap())
         .serve(app.into_make_service())
-        .await?
+        .await
         .unwrap();
 }
- 
+
+async fn create_todos() {
+    println!("create-todo page");
+    // todo: something from frontend triggers this function
+}
+
+async fn delete_todos() {
+    println!("delete-todo page");
+    // deletes data from db
+    // todo: something from frontend triggers this function
+}
+
+async fn view_todos(){
+    println!("view-todos page");
+    // todo: something from frontend triggers this function
+}
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,5 +1,25 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+
 fn main() {
     println!("Frontend!");
-
+    dioxus_web::launch(App);
 }
 
+
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        ul {
+            li {
+                "Create Todos"
+            },
+            li {
+                "Delete Todos"
+            }, 
+            li {
+                "Views Todos"
+            }
+        }
+    })
+}
